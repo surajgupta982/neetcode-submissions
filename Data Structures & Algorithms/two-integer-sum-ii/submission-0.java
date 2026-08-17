0df53914f525dd49f58634ce61a5747924cdb0b1
Suraj Gupta <guptasuraj974@gmail.com>
2026-08-17 21:01:29 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        Map<Integer, Integer> mp = new HashMap<>();
+
+        int n=numbers.length;
+        for(int i=0;i<n;i++)
+        {   
+            if(mp.containsKey(target-numbers[i])){
+                //return new int[mp.get(target-numbers[i]), i];
+                int[] res = new int[2];
+                res[0] = mp.get(target-numbers[i])+1;
+                res[1] = i+1;
+                return res;
+            }
+            else
+                mp.put(numbers[i], i);
+        }
+
+        return new int[0];
+
+    }
+}
