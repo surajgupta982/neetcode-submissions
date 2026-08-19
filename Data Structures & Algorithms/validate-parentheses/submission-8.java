0f046beffa88f06d17557d65592449881977bfd1
Suraj Gupta <guptasuraj974@gmail.com>
2026-08-19 16:46:02 +0530

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> st = new Stack<>();
+
+        int i=0;
+        int n = s.length();
+
+        while(i<n)
+        {
+            char c = s.charAt(i);
+            if(c=='(' || c=='{' || c=='[')
+                st.push(c);
+            else
+            {
+                if(st.isEmpty())
+                    return false;
+                if( ( c==')' && st.peek()=='(' ) ||
+                    ( c=='}' && st.peek()=='{' ) ||
+                    ( c==']' && st.peek()=='[' ) 
+                  )
+                    st.pop();
+                else
+                    return false;
+            }
+            i++;
+        }
+
+        return st.isEmpty();
+
+    }
+}
